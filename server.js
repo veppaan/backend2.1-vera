12a6864d817df30cc8f6956811064eba592e971e
Vera Kippel <veki2400@student.miun.se>
2025-04-24 17:03:25 +0200

    mer funktionalitet och utskrifter i delete-metod

--- server.js
+++ server.js
@@ -73,15 +73,16 @@ app.delete("/resume/delete/:id", (req, res) => {
     let id = req.params.id;
 
     //Delete job
-    db.run("DELETE FROM resume WHERE id=?;", id, (err) => {
+    db.run("DELETE FROM resume WHERE id=?;", id, function (err) {
         if(err){
             console.error(err.message);
         }
-        let deletedJob = {
-            id: id
+        console.log(`${this.changes}`);
+        if(this.changes === 0){
+            res.status(404).json ({ error: `Job with id (${id}) does not exist`});
+            return;
         }
-    
-        res.json({ message: "Job deleted with id: ", id});
+        res.status(200).json({ message: "Job deleted with id: " + id});
     })
 })
 
