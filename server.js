752574d3e5d53e365a0c138d33216791921168ee
Vera Kippel <veki2400@student.miun.se>
2025-04-24 15:50:54 +0200

    Lägger in GET-metod

--- server.js
+++ server.js
@@ -3,18 +3,28 @@ const app = express();
 const port = process.env.PORT || 3000;
 //require("dotenv").config();
 //Databas
-//const sqlite3 = require("sqlite3").verbose();
-//const db = new sqlite3.Database(process.env.DB_PATH);
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(process.env.DB_PATH);
 
 //Routes
 
 //Hämta alla rader i resume-databas
-app.get("/resume", (req, res) => {
+app.get("/", (req, res) => {
     res.json({message: "Get resume"})
 });
-/* app.get("/resume/all", (req, res) => {
-    res.json({message: "Get resumes"})
-}) */
+//Hämta resume-tabellens värden GET
+app.get("/resume", (req, res) => {
+    db.all("SELECT * FROM resume;", (err, row) => {
+        if(err){
+            console.error(err.message);
+        }
+        res.render("resume", {
+            error: "",
+            row: row
+        })
+    })
+}); 
+
 
 app.listen(port, () => {
     console.log("Server running on port: " + port);
